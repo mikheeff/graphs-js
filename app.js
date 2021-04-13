03d495cf4bdb5b443da741faaad8a3cb2b25aa42
Pavel Mikheev <pavel.mikheev@t-systems.com>
2021-04-13 15:05:06 +0300

    code refactoring

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const PADDING = 40;
 const DPI_WIDTH = WIDTH * 2;
 const DPI_HEIGHT = HEIGHT * 2;
 const VIEW_HEIGHT = DPI_HEIGHT - PADDING * 2
+const VIEW_WIDTH = DPI_WIDTH;
 const ROWS_COUNT = 5;
 
 function chart(canvas, data) {
@@ -15,8 +16,33 @@ function chart(canvas, data) {
 
   const [yMin, yMax] = computeBoundaries(data);
   const yRatio = VIEW_HEIGHT / (yMax - yMin);
+  const xRatio = VIEW_WIDTH / (data.columns[0].length - 2)
 
-  // === y axis
+  const yData = data.columns.filter(col => data.types[col[0]] === 'line');
+  const [xData] = data.columns.filter(col => data.types[col[0]] !== 'line');
+
+  yAxis(ctx, yMin, yMax);
+  xAxis(ctx, xData, xRatio);
+
+
+  yData.map(toCoords(xRatio, yRatio))
+    .forEach((coords, idx) => {
+      const color = data.colors[yData[idx][0]]
+      line(ctx, coords, {color})
+    })
+}
+
+function toCoords(xRation, yRatio) {
+  return (col) =>
+    col.map((y, i) => [
+      Math.floor((i - 1) * xRation),
+      Math.floor(DPI_HEIGHT - PADDING - y * yRatio)
+    ])
+      .filter((_, i) => i !== 0);
+
+}
+
+function yAxis(ctx, yMin, yMax) {
   const step = VIEW_HEIGHT / ROWS_COUNT;
   const textStep = (yMax - yMin) / ROWS_COUNT;
 
@@ -26,7 +52,7 @@ function chart(canvas, data) {
   ctx.fillStyle = '#96a2aa'
   for (let i = 1; i <= ROWS_COUNT; i++) {
     const y = step * i;
-    const text = yMax - textStep * i;
+    const text = Math.round(yMax - textStep * i);
     ctx.fillText(text.toString(), 5, y + PADDING - 10)
     ctx.moveTo(0, y + PADDING);
     ctx.lineTo(DPI_WIDTH, y + PADDING)
@@ -34,13 +60,29 @@ function chart(canvas, data) {
 
   ctx.stroke();
   ctx.closePath();
-  // ===
+}
+
+function xAxis(ctx, data, xRatio) {
+  const colsCount = 6;
+  const step = Math.round(data.length / colsCount);
 
   ctx.beginPath();
+
+  for (let i = 1; i < data.length; i+=step) {
+    const text = toDate(data[i]);
+    const x = i * xRatio
+    ctx.fillText(text.toString(), x, DPI_HEIGHT - 10);
+  }
+
+  ctx.closePath();
+}
+
+function line(ctx, coords, { color }) {
+  ctx.beginPath();
   ctx.lineWidth = 4;
-  ctx.strokeStyle = '#ff0000'
-  for (const [x, y] of data) {
-    ctx.lineTo(x, DPI_HEIGHT - PADDING - y * yRatio);
+  ctx.strokeStyle = color;
+  for (const [x, y] of coords) {
+    ctx.lineTo(x, y);
   }
   ctx.stroke();
   ctx.closePath();
@@ -66,15 +108,16 @@ function computeBoundaries({ columns, types }) {
 
 
       if (min > col[1]) min = col[1];
-      if (max < y) max = y;
-  })
-  // for (const [, y] of data) {
+      if (max < col[1]) max = col[1];
 
-  //
-  //   if (min > y) min = y;
-  //   if (max < y) max = y;
-  //
-  // }
+
+    for (let i = 2; i < col.length; i++) {
+
+      if (min > col[i]) min = col[i];
+      if (max < col[i]) max = col[i];
+
+    }
+  })
 
   return [min, max]
 }
@@ -444,4 +487,24 @@ function getChartData() {
       },
     },
   ][0]
+}
+
+function toDate(timestamp) {
+  const shortMonths = [
+    'Jan',
+    'Feb',
+    'Mar',
+    'Apr',
+    'May',
+    'Jun',
+    'Jul',
+    'Aug',
+    'Sep',
+    'Oct',
+    'Nov',
+    'Dec',
+  ];
+
+  const date = new Date(timestamp);
+  return `${shortMonths[date.getMonth()]} ${date.getDate()}`
 }
